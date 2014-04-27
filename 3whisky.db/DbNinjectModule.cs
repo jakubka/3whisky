b1543e7d2415aa320ea4f4aa5e4bc86c5196bd85
Jakub Kadlubiec <jakub.kadlubiec@gmail.com>
2014-04-27 19:52:00 +0200

    DI2

---
+++ 3whisky.db/DbNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3whisky.db
+{
+    public class DbNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
---
+++ 3whisky.web/WebNinjectModule.cs
@@ -0,0 +1,17 @@
+﻿using _3whisky.configuration;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _3whisky.web
+{
+    public class WebNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDbConfiguration>().To<WebConfigConfiguration>();
+        }
+    }
+}
