3ad136191b77e0414e047b8a4d7775c3a34866bf
jakubka <jakub.kadlubiec@gmail.com>
2014-04-27 15:46:21 +0200

    MVC solution

---
+++ 3whisky.web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3whisky.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Index/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
