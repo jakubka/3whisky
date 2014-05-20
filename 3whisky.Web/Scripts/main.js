370873c3b622edbc35246d2ae1823abbfe91568a
Roman Mazur <roman.mazur@hotmail.com>
2014-05-20 22:07:46 +0200

    Zakladni validace formulare (bez stylu a bez reseni validacnich hlasek).

---
+++ 3whisky.Web/Scripts/main.js
@@ -0,0 +1,25 @@
+﻿(function (ng) {
+    'use strict';
+
+    var ProductDetailController = ['$scope', function ($scope) {
+        $scope.order = {};
+        $scope.personalPaymentMethodText = "Dobírka";
+        $scope.showShippingAddressInput = true;
+
+        $scope.shipmentMethodChanged = function (currentShipmentMethod) {
+            if (currentShipmentMethod === 'CeskaPosta') {
+                $scope.personalPaymentMethodText = "Dobírka";
+                $scope.showShippingAddressInput = true;
+            } else {
+                $scope.personalPaymentMethodText = "Hotově při odběru";
+                $scope.showShippingAddressInput = false;
+            }
+        }
+    }];
+
+    ng.module('3whisky', [])
+        .controller('ProductDetailController', ProductDetailController);
+
+    ng.bootstrap(window.document, ['3whisky']);
+
+}(window.angular));
